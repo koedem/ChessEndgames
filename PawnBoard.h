79282c66754da39cb55e4cf071a9e5a955f2df91
Kolja Kühn <kolja@heck-kuehn.de>
2022-08-18 01:47:21 +0200

    Initial commit. Basic move generator and search, no en passant or TT.

---
+++ PawnBoard.h
@@ -0,0 +1,144 @@
+//
+// Created by kolja on 8/4/22.
+//
+
+#ifndef CHESSENDGAMES_PAWNBOARD_H
+#define CHESSENDGAMES_PAWNBOARD_H
+
+#include <cstdint>
+#include <vector>
+#include <cstring>
+#include <bit>
+
+class PawnBoard {
+
+
+public:
+/**
+     * bit order: a2, b2, ..., h2, a3, ..., h7 from lowest to highest bit, i.e. & 1 gives a2 occupancy
+     *     could in theory be changed for wider board; note en_passant code though
+     */
+    uint64_t white_pieces = 0x1F;
+    uint64_t black_pieces = 0x1F0000000000;
+
+private:
+    uint64_t all_pieces = white_pieces | black_pieces;
+    bool to_move = true;
+    uint64_t hash;
+
+    static constexpr int number_of_files = 8; // a to h are 8 for normal chess, note en_passant code if changed
+    static constexpr uint64_t full_row = (1L << number_of_files) - 1;
+    static constexpr int changes[] = { number_of_files, number_of_files * 2, number_of_files - 1, number_of_files + 1 };
+
+
+public:
+
+    /**
+     *
+     * @tparam WHITE the side who just moved
+     * @return
+     */
+    template<bool WHITE>
+    int eval() {
+        int eval;
+        if constexpr (WHITE) {
+            eval = std::popcount(white_pieces) - std::popcount(black_pieces); // material
+            if ((white_pieces & (full_row << (number_of_files * 5))) != 0) {
+                eval += 128;
+            }
+        } else {
+            eval = std::popcount(black_pieces) - std::popcount(white_pieces); // material
+            if ((black_pieces & full_row) != 0) {
+                eval += 128;
+            }
+        }
+        return eval;
+    }
+
+    /**
+     *
+     * @tparam WHITE who to move it is before the move
+     * @param move
+     */
+    template<bool WHITE>
+    void make_move(uint32_t move) {
+        uint64_t start_square = move & 63;
+        uint64_t move_type = move >> 6;
+        uint64_t change = 1L << start_square;
+        if constexpr (WHITE) {
+            uint64_t destination_square = start_square + changes[move_type];
+            white_pieces ^= change | 1L << destination_square;
+            if (move_type >= 2) { // capture
+                black_pieces ^= 1L << destination_square;
+            } else {
+                change ^= 1L << destination_square;
+            }
+        } else {
+            uint64_t destination_square = start_square - changes[move_type];
+            black_pieces ^= change | 1L << destination_square;
+            if (move_type >= 2) { // capture
+                white_pieces ^= 1L << destination_square;
+            } else {
+                change ^= 1L << destination_square;
+            }
+        }
+        all_pieces ^= change;
+        to_move = !to_move;
+    }
+
+    /**
+     *
+     * @tparam WHITE who to move it was before the move, i.e. whose move it will be after the unmove
+     * @param move
+     */
+    template<bool WHITE>
+    void unmake_move(uint32_t move) {
+        make_move<WHITE>(move); // ye, actually I think
+    }
+
+    template<bool WHITE>
+    void generate_moves(std::array<uint32_t, 25>& moves) {
+        moves[0] = 0;
+        if constexpr (WHITE) {
+            uint64_t pieces = white_pieces;
+            while (pieces != 0) {
+                int pos = 63 - __builtin_clzl(pieces);
+                if ((all_pieces & (1L << (pos + number_of_files))) == 0) {
+                    moves[++moves[0]] = pos;
+                    if (pos < number_of_files && (all_pieces & (1L << (pos + number_of_files * 2))) == 0) {
+                        moves[++moves[0]] = pos | (1 << 6);
+                    }
+                }
+                if ((black_pieces & (1L << (pos + (number_of_files - 1)))) != 0) {
+                    moves[++moves[0]] = pos | (2 << 6);
+                }
+                if ((black_pieces & (1L << (pos + (number_of_files + 1)))) != 0) {
+                    moves[++moves[0]] = pos | (3 << 6);
+                }
+                pieces &= ~(1L << pos);
+            }
+        } else {
+            uint64_t pieces = black_pieces;
+            while (pieces != 0) {
+                int pos = ffsl(pieces) - 1;
+                // TODO captures
+                if ((all_pieces & (1L << (pos - number_of_files))) == 0) {
+                    moves[++moves[0]] = pos;
+                    if (pos >= number_of_files * 5 && (all_pieces & (1L << (pos - number_of_files * 2))) == 0) {
+                        moves[++moves[0]] = pos | (1 << 6);
+                    }
+                }
+                if ((white_pieces & (1L << (pos - (number_of_files - 1)))) != 0) {
+                    moves[++moves[0]] = pos | (2 << 6);
+                }
+                if ((white_pieces & (1L << (pos - (number_of_files + 1)))) != 0) {
+                    moves[++moves[0]] = pos | (3 << 6);
+                }
+                pieces &= ~(1L << pos);
+            }
+        }
+    }
+};
+
+
+#endif //CHESSENDGAMES_PAWNBOARD_H
---
+++ main.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <array>
+#include <unordered_set>
+#include "PawnBoard.h"
+
+PawnBoard board;
+
+std::array<std::array<uint32_t , 25>, 100> moves_per_depth;
+
+std::vector<std::array<uint64_t, 4>> fake_hash_table;
+
+int temp_best_move;
+
+void find_hash(uint64_t white, uint64_t black, uint64_t depth) {
+    for (std::array<uint64_t, 4>& entry : fake_hash_table) {
+        if (entry[0] == white && entry[1] == black && entry[2] == depth) {
+            std::cout << entry[3] << std::endl;
+            break;
+        }
+    }
+}
+
+template<bool WHITE>
+uint64_t perft(int depth) {
+    if (depth == 0) {
+        return 1;
+    }
+    uint64_t nodes = 0;
+    board.generate_moves<WHITE>(moves_per_depth[depth]);
+    for (int i = 1; i <= moves_per_depth[depth][0]; i++) {
+        uint32_t move = moves_per_depth[depth][i];
+        board.make_move<WHITE>(move);
+        nodes += perft<!WHITE>(depth - 1);
+        board.unmake_move<WHITE>(move);
+    }
+    return nodes;
+}
+
+template<bool WHITE, uint32_t DEPTH>
+int negaMax(int alpha, int beta) {
+    board.generate_moves<WHITE>(moves_per_depth[DEPTH]);
+    if (moves_per_depth[DEPTH][0] == 0) { // we have no legal moves, i.e. got stalemated
+        return board.eval<WHITE>() - 8; // -8 as eval term for stalemated
+    } else {
+        uint32_t best_move = 255; // -1 byte as non move
+        for (int i = 1; i <= moves_per_depth[DEPTH][0]; i++) {
+            uint32_t move = moves_per_depth[DEPTH][i];
+            board.make_move<WHITE>(move);
+            int eval = board.template eval<WHITE>();
+            if (eval > 50) {
+                // yeah, maybe templating depth wasn't a good idea, requiring this...
+            } else if constexpr (DEPTH > 1) {
+                eval = -negaMax<!WHITE, DEPTH - 1>(-beta, -alpha);
+            }
+            board.unmake_move<WHITE>(move);
+            if (eval > alpha) {
+                alpha = eval;
+                best_move = move;
+                if (alpha >= beta) {
+                    alpha = beta;
+                    break;
+                }
+            }
+        }
+        if (DEPTH > 10) {
+            fake_hash_table.template emplace_back(
+                    std::array<uint64_t, 4>{board.white_pieces, board.black_pieces, DEPTH + (WHITE ? 0 : 100),
+                                            best_move});
+        }
+    }
+    return alpha;
+}
+
+template<bool WHITE>
+void setup();
+
+template <bool WHITE>
+void search() {
+    std::cout << "Eval " << 1 << " is: " << negaMax<WHITE, 1>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 2 << " is: " << negaMax<WHITE, 2>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 3 << " is: " << negaMax<WHITE, 3>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 4 << " is: " << negaMax<WHITE, 4>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 5 << " is: " << negaMax<WHITE, 5>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 6 << " is: " << negaMax<WHITE, 6>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 7 << " is: " << negaMax<WHITE, 7>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 8 << " is: " << negaMax<WHITE, 8>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 9 << " is: " << negaMax<WHITE, 9>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 10 << " is: " << negaMax<WHITE, 10>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 11 << " is: " << negaMax<WHITE, 11>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 12 << " is: " << negaMax<WHITE, 12>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 13 << " is: " << negaMax<WHITE, 13>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 14 << " is: " << negaMax<WHITE, 14>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 15 << " is: " << negaMax<WHITE, 15>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 16 << " is: " << negaMax<WHITE, 16>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 17 << " is: " << negaMax<WHITE, 17>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 18 << " is: " << negaMax<WHITE, 18>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 19 << " is: " << negaMax<WHITE, 19>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 20 << " is: " << negaMax<WHITE, 20>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 21 << " is: " << negaMax<WHITE, 21>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 22 << " is: " << negaMax<WHITE, 22>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 23 << " is: " << negaMax<WHITE, 23>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 24 << " is: " << negaMax<WHITE, 24>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 25 << " is: " << negaMax<WHITE, 25>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 26 << " is: " << negaMax<WHITE, 26>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 27 << " is: " << negaMax<WHITE, 27>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 28 << " is: " << negaMax<WHITE, 28>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Eval " << 29 << " is: " << negaMax<WHITE, 29>(-200, 200) << ", move " << temp_best_move << std::endl;
+    std::cout << "Table size " << fake_hash_table.size() << std::endl;
+    setup<WHITE>();
+}
+
+template <bool WHITE>
+void setup() {
+    std::string command;
+    std::cin >> command;
+    if (command == "go") {
+        search<WHITE>();
+    } else {
+        if (command == "hash") {
+            std::string depth;
+            std::cin >> depth;
+            find_hash(board.white_pieces, board.black_pieces, std::stoul(depth));
+            setup<WHITE>();
+        } else {
+            board.template make_move<WHITE>(std::stoi(command));
+            setup<!WHITE>();
+        }
+    }
+}
+
+int main() {
+    //setup<true>();
+
+    for (int i = 0; i < 20; i++) {
+        std::cout << "Perft " << i << " is: " << perft<true>(i) << std::endl;
+    }
+    return 0;
+}
